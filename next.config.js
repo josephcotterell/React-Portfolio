507d0dd4bd53737d69f489dbc32185a6e99fbf04
joseph <josephcotterell@hotmail.co.uk>
2023-04-27 12:28:31 +0100

    fixed next.config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
 };
-const withImages = require("next-images");
-module.exports = withImages();
 
 module.exports = nextConfig;
